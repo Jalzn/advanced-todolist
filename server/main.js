14cd2c2fa6aab24168df486709b8596ada874d6b
Jalzn <jalmirfsjr@gmail.com>
2025-05-26 09:12:58 -0300

    remove old seed in startup

--- server/main.js
+++ server/main.js
@@ -1,13 +1,6 @@
 import { Meteor } from "meteor/meteor";
 
-import { TasksCollection } from "../imports/api/TasksCollection";
 import "../imports/api/taskPublications";
 import "../imports/api/taskMethods";
 
-Meteor.startup(async () => {
-  if ((await TasksCollection.find().countAsync()) === 0) {
-    [
-      { name: "Hello world", description: "Hello world", date: "Today" },
-    ].forEach((task) => TasksCollection.insertAsync(task));
-  }
-});
+Meteor.startup(async () => {});
